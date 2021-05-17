f12c3be1894006ab73cd717bc9ea635f875acff7
VivekBhardwaj <50169280+vivekbhardwaj-1201@users.noreply.github.com>
2021-05-17 09:52:06 +0530

    Add files via upload

---
+++ build/precache-manifest.8092493c9763238f3f19119ca6bffb2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025786d9f4f543791d14a6672f351292",
+    "url": "/invictus/index.html"
+  },
+  {
+    "revision": "8849ab6991f09ab879d8",
+    "url": "/invictus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc5798a7f49f107c84ea",
+    "url": "/invictus/static/js/2.88ea89c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invictus/static/js/2.88ea89c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8849ab6991f09ab879d8",
+    "url": "/invictus/static/js/main.540accad.chunk.js"
+  },
+  {
+    "revision": "056da24d4f0b21e322c9",
+    "url": "/invictus/static/js/runtime-main.a639b245.js"
+  }
+]);
